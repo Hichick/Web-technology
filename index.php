7d6384432552dfc7a7f145d732288d995ee554d7
Hichick <glebkalin@mail.ru>
2015-05-17 23:58:06 +0500

    Лабораторная работа №1
    Перезалита лабораторая работа №1, смена тематики.
    1)  Была создана страница с помощью html5 и css
    2)  Были использованы семантические теги
    3)  В таблице стилей были описаны стили всех использованных классов и
    почти всех тегов
    4)  Была успешно пройдена валидация на сайте http://validator.w3.org
    5)  Были сделаны скришоты страницы с css и без css

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="author" content="Hichas" />
+
+	<title>Фан сайт по ККИ Hearthstone</title>
+    
+    <link rel="stylesheet" type="text/css" href="style.css" />
+        
+</head>
+<body>
+
+<header>
+<div class="logo">
+<img src="img/logo.png" alt="HearthStone" />
+</div>
+
+</header>
+
+<div class="main">
+
+<nav>
+        <a href="/">Главная</a>  <a href="#">Воин</a> <a href="#">Маг</a>  <a href="#">Охотник</a> <a href="#">Шаман</a>  <a href="#">Чернокнижник</a> <a href="#">Разбойник</a> <a href="#">Жрец</a> <a href="#">Друид</a> <a href="#">Паладин</a>
+</nav>
+
+<div class="content">
+
+  <p>Hearthstone: Heroes of Warcraft — компьютерная коллекционная карточная онлайн-игра по мотивам вселенной Warcraft, разработанная Blizzard Entertainment и распространяемая по модели free-to-play. В игре Blizzard поддерживается Battle.net, и загрузка происходит через лаунчер от Blizzard. Продукт выпущен на PC, OS X, iOS и Android. Версия игры для iPad доступна для загрузки в App Store. Версия для Android-планшетов вышла 16 декабря 2014 года. На момент выхода, загрузка из Google Play была доступна только для Канады, Новой Зеландии и Австралии. С 18 декабря 2014 года стала доступна для Европы.</p>
+  
+  <a href="">Последние колоды:</a>
+  <hr />
+  <div class="deck">
+  
+  <div class="cover_mask"></div>
+  <div class="cover_img"><img  width="272" height="145"  src="img/first_news.jpg" alt="Новый Малигос Разбойник" /></div>
+  <div class="deck_content">
+  <a href="">Новый Малигос Разбойник</a>
+  <hr/>
+  
+  Представляю вам колоду Миракл Малигос Разбойника, при помощи которой я взял топ-23 Легенды в апрельском сезоне 2015 года. 
+  </div>
+  
+  </div>
+  
+   
+  <div class="deck">
+  
+  <div class="cover_mask"></div>
+  <div class="cover_img"><img  width="272" height="145"  src="img/first_news.jpg" alt="Новый Малигос Разбойник"  /></div>
+  <div class="deck_content">
+  <a href="">Новый Малигос Разбойник</a>
+  <hr/>
+  
+  Представляю вам колоду Миракл Малигос Разбойника, при помощи которой я взял топ-23 Легенды в апрельском сезоне 2015 года. 
+  </div>
+  
+  </div>
+  
+ 
+
+  <a href="">Самые обсуждаемые:</a>
+  <hr />
+  <div class="deck">
+  
+  <div class="cover_mask"></div>
+  <div class="cover_img"><img  width="272" height="145"  src="img/first_news.jpg" alt="Новый Малигос Разбойник" /></div>
+  <div class="deck_content">
+  <a href="">Новый Малигос Разбойник</a>
+  <hr/>
+  
+  Представляю вам колоду Миракл Малигос Разбойника, при помощи которой я взял топ-23 Легенды в апрельском сезоне 2015 года. 
+  </div>
+  
+  </div>
+  
+   
+  <div class="deck">
+  
+  <div class="cover_mask"></div>
+  <div class="cover_img"><img  width="272" height="145"  src="img/first_news.jpg" alt="Новый Малигос Разбойник"  /></div>
+  <div class="deck_content">
+  <a href="">Новый Малигос Разбойник</a>
+  <hr/>
+  
+  Представляю вам колоду Миракл Малигос Разбойника, при помощи которой я взял топ-23 Легенды в апрельском сезоне 2015 года. 
+  </div>
+  
+  </div>
+  
+  
+  
+  
+  
+</div>
+
+</div>
+
+<footer>
+
+</footer>
+
+
+</body>
+</html>
